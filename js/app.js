9118cff4f83ccd6bc2143c157c5a750a4d746644
Ivy <ivy.cm.wong@gmail.com>
2014-12-02 17:49:36 -0800

    Previous files from old one

---
+++ js/app.js
@@ -0,0 +1,27 @@
+/**
+ * Created by Shrinivas on 11/18/2014.
+ */
+
+"use strict";
+
+$(document).ready(function() {
+    //initial
+
+    //handle menu clicks
+    $('ul#nav li a').click(function() {
+        var page = $(this).attr('href');
+        $('#content').load(page + '.html');
+        return false;
+    });
+
+
+//    document.getElementById("navBar").innerHTML =
+//        "<ul id='menu'>" +
+//        "<li><a href='index.html'>Home</a></li>" +
+//        "<li><a href='History.html'>History</a></li>" +
+//        "<li><a href='Events.html'>Events</a></li>" +
+//        "<li><a href='Gallery.html'>Gallery</a></li>" +
+//        "</ul>";
+////document.getElementById("trademark").innerHTML =
+////    ""
+});
