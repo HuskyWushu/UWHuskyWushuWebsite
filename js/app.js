b384ef5920157929cc6b139e9178f21d03a3c46d
Michael Dang <michael.w.dang@gmail.com>
2014-12-02 20:56:30 -0800

    Fixed header spacing

--- js/app.js
+++ js/app.js
@@ -6,20 +6,11 @@
 
 $(document).ready(function() {
     $('#content').load('home.html');
-
+    $('#headerPlaceholder').height( $('#navigationBar').outerHeight());
     //handle menu clicks
     $('#main-nav ul.nav li a').click(function() {
         var page = $(this).attr('href');
         $('#content').load(page + '.html');
         return false;
     });
-//    document.getElementById("navBar").innerHTML =
-//        "<ul id='menu'>" +
-//        "<li><a href='index.html'>Home</a></li>" +
-//        "<li><a href='history.html'>History</a></li>" +
-//        "<li><a href='events.html'>Events</a></li>" +
-//        "<li><a href='gallery.html'>Gallery</a></li>" +
-//        "</ul>";
-////document.getElementById("trademark").innerHTML =
-////    ""
 });
