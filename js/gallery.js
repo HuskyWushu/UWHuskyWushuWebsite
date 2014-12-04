d30f1b681fa6ca10b1a5d0fb3820c6fc906f915a
Shrinivas Kopparam Ramanath <shrini@uw.edu>
2014-12-03 20:12:32 -0800

    Adding pictures into array

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,34 @@
 /**
  * Created by Ivy on 12/3/14.
  */
+    $(document).ready(function() {
+        var img = "../img/GalleryImages/2012";
+        var fileextension = [".png", ".jpg"];
+        $(data).find("a:contains(" + (fileextension[0]) + "), a:contains(" + (fileextension[1]) + ")").each(function () {
+            $.ajax({
+                //This will retrieve the contents of the folder if the folder is configured as 'browsable'
+                url: dir,
+                success: function (data) {
+                    //Lsit all png file names in the page
+                    $(data).find("a:contains(" + fileextension + ")").each(function () {
+                        var filename = this.href.replace(window.location.host, "").replace("http:///", "");
+                        $("body").append($("<img src=" + dir + filename + "></img>"));
+                    });
+                }
+            });
+        });
+
+        var gallery = $('#gallery');
+        var row = $(document.createElement('div'));
+        var img;
+        _.forEach(tilePairs, function(tile, elemIndex) {
+            if(elemIndex > 0 && 0 == elemIndex % 4) {
+                gallery.append(row);
+                row = $(document.createElement('div'));
+            }
+            img = $(document.createElement('img'));
+            row.append(img);
+        });
+        gallery.append(row);
+    });
+
