460ff920bfdbc77ce0bbd3ecae4e940af7df97cb
Michael Dang <michael.w.dang@gmail.com>
2014-12-06 23:29:26 -0800

    Fixed navbar

--- js/app.js
+++ js/app.js
@@ -5,22 +5,6 @@
 "use strict";
 
 $(document).ready(function() {
-    //reveal homepage
-    var contentDiv = $('#content');
-    contentDiv.load('home.html');
-    contentDiv.css("display", "none");
-    contentDiv.fadeIn(2000);
-
-    $('#headerPlaceholder').height($('#navigationBar').outerHeight());
-    //handle menu clicks
-    $('#main-nav ul.nav li a').click(function() {
-        var contentDiv = $('#content');
-        contentDiv.load($(this).attr('href'), function() {
-            contentDiv.hide().fadeIn(500);
-        });
-
-        return false;
-    });
-
-
+    //load header
+    $('#head').load('header.html');
 });
