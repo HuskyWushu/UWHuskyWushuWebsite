0c20968c8d118858bf74f1517ce31ef4d942d916
jmdevera <jmdevera21@gmail.com>
2014-12-07 20:44:51 -0800

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	home.html
    	js/home.js

--- js/home.js
+++ js/home.js
@@ -1,19 +1,18 @@
 "use strict";
-
     // loads event data to display on Home
     (window.onload = function () {
         makeAjax(updateEvents);
     });
-    var event1 = document.getElementById("event1");
-    var event2 = document.getElementById("event2");
-    var event3 = document.getElementById("event3");
-
     // This function creates an Ajax request to a given url and
     // executes a given function func with the response.
     function makeAjax(func) {
         var CAL_API = "https://www.googleapis.com/calendar/v3/calendars/";
         var CAL_ID = "3218n1qbhtm3fe5gims1fgciso@group.calendar.google.com/";
         var API_KEY = "AIzaSyC59NOWzcnm5DCri2-BF9gftvgSq_8isUc";
+        var event1 = document.getElementById("event1");
+        var event2 = document.getElementById("event2");
+        var event3 = document.getElementById("event3");
+
         if (event1 && event2 && event3) {
             event1.innerHTML = "test1";
             event2.innerHTML = "foo";
