54706ec74c5fd666546c7c33455fd050bd6ce76a
Ivy <ivy.cm.wong@gmail.com>
2014-12-20 21:04:05 -0800

    Using Skrollr: background images vanishing and not working properly.

--- js/parallaxSkroller.js
+++ js/parallaxSkroller.js
@@ -1,11 +1,18 @@
 /**
- * Created by Ivy on 12/16/2014.
- */
+* Created by Ivy on 12/16/2014.
+*/
 
 "Use Strict"
 
 $(document).ready(function($) {
-    skrollr.init(function() {
-        smoothScrolling: false
+    $('#head').load('header.html');
+    var s = skrollr.init({
+        forceHeight: false,
+        smoothScrolling: false,
+        mobileDeceleration: 0.004
+    });
+
+    $(window).scroll(function() {
+        s.refresh();
     });
 });
